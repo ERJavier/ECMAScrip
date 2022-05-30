69c415fef09d36eb6f365f2a4a4468e2d88c02d0
Javier Cepeda <erjavierc@gmail.com>
2022-05-29 21:49:42 -0400

    Clase4

--- src/es6/index.js
+++ src/es6/index.js
@@ -67,3 +67,51 @@ console.log(globalVar);
 var a = 'b';
 a = 'a';
 console.log(a);
+
+//es6
+let name = 'javier';
+let age = '32';
+
+//es5
+obj = { name: name, age: age};
+
+obj2 = { name, age};
+console.log(obj2);
+
+const names = [
+    {name: 'javi', age: 28},
+    {name: 'Kirbel', age: 22}
+]
+
+let listOfNames = names.map(function (item){
+    console.log(item.name);
+});
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (name, age, country) => {
+    ...
+}
+
+const listOfNames4 = name => {
+    ...
+}
+
+const square = num => num * num;
+
+//PROMESA
+
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (false){
+            resolve('Hey!')
+        }else {
+            reject('Ups!!');
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(() => console.log('Hola'))
+    .catch(error => console.log(error));
