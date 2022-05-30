cca52a17c3b3870080e9f0f6985753383305eae2
Javier Cepeda <erjavierc@gmail.com>
2022-05-30 13:48:24 -0400

    Clase5

--- src/es6/index.js
+++ src/es6/index.js
@@ -115,3 +115,44 @@ helloPromise()
     .then(response => console.log(response))
     .then(() => console.log('Hola'))
     .catch(error => console.log(error));
+
+    //CLASES 
+
+    class calculator {
+        constructor(){
+            this.valueA = 0;
+            this.valueB = 0;
+        }
+        sum(valueA, valueB) {
+            this.valueA = valueA;
+            this.valueB = valueB;
+            return this.valueA + this.valueB;
+        }
+    }
+
+    const calc = new calculator();
+    console.log(calc.sum(2,2));
+
+// IMPORT --- EXPORT
+
+import{hello} from './module';
+
+hello();
+
+console.log(hello());
+
+//GENERATORS
+
+function* helloWorld(){
+    if (true){
+        yield 'Hello, ';
+    }
+    if(true){
+        yield 'World';
+    }
+};
+const geneatorHello = helloWorld();
+ 
+console.log(geneatorHello.next().value);
+console.log(geneatorHello.next().value);
+console.log(geneatorHello.next().value);
