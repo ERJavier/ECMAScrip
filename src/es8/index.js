6e49dcc414879047e05a353e63b9febc5f920304
Javier Cepeda <erjavierc@gmail.com>
2022-05-30 14:44:30 -0400

    ECMAScript8

--- src/es8/index.js
+++ src/es8/index.js
@@ -27,3 +27,29 @@ console.log('food'.padEnd(12, ' ------'));
 const obj = {
     name: 'javier',
 }
+
+
+const HelloWorld = () => {
+    return new Promise((resolve, reject) =>{
+        (true)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await HelloWorld();
+    console.log(hello)
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await HelloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+anotherFunction();
